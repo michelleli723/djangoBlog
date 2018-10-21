2e3d8d5c1ba51d32b4e8ba7b94a6f3eb83c9a41e
Michelle Li <michelleli94237@gmail.com>
2018-10-21 16:27:59 -0400

    added post models and added it to the admin gui

--- djangoBlog/blog/models.py
+++ djangoBlog/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+	#title field that's a characterfield(max_length=100)
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	date_posted = models.DateTimeField(default=timezone.now)
+	#if user deletes account, it will delete all their posts with cascade
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
+
+
