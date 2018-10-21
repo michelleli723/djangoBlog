f2e6c8b31da6db4299749503554b8a6b3276c45e
Michelle Li <michelleli94237@gmail.com>
2018-10-21 16:42:43 -0400

    added the users app into the project's installed apps

--- djangoBlog/djangoBlog/settings.py
+++ djangoBlog/djangoBlog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [ #to find databases, models 
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
